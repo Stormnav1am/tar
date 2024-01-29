9b597883f266d633c9826fd6490cced56af51476
friedchicken <112839488+Stormnav1am@users.noreply.github.com>
2024-01-29 15:51:42 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Send Email</title>
+    </head>
+    <body>
+        <form class="" action="send.php" method="post">
+            Message <input type="text" name="message"> <br>
+            <button type="submit" name="send">Send</button>
+        </form>
+    </body>
+</html>
